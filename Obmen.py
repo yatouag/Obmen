57e6826129e5d250ece54963033c46691b6108bd
Stas <yatouag@gmail.com>
2024-11-06 11:52:28 +0300

    Созданно главное окно прогрвммы

--- Obmen.py
+++ Obmen.py
@@ -1,9 +1,20 @@
 import requests
 import json
-import pprint
+from tkinter import *
+from tkinter import messagebox as mb
 
-result = requests.get("https://open.er-api.com/v6/latest/USD")
-data = json.loads(result.text)
-p = pprint.PrettyPrinter(indent=4)
 
-p.pprint(data)
+def
+
+window = Tk()
+window.title("Курс обмена валют")
+window.geometry("360x180")
+
+Label(text="Введите код валюты").pack(padx=10, pady=10)
+
+entry = Entry()
+entry.pack(padx=10, pady=10)
+
+Button(text="Получить курс обмена к USD", command=exchange).pack(padx=10, pady=10)
+
+window.mainloop()
