64131ecf83733c3e5419f901052408753cb41d9d
Stas <yatouag@gmail.com>
2024-11-06 12:19:27 +0300

    Изменен формат вывода курса. Три знака после запятой.

--- Obmen.py
+++ Obmen.py
@@ -14,7 +14,7 @@ def exchange():
             data = response.json()
             if code in data["rates"]:
                 exchange_rate = data["rates"][code]
-                mb.showinfo("Курс обмена", f"Курс: {exchange_rate} {code} за 1 USD")
+                mb.showinfo("Курс обмена", f"Курс: {exchange_rate: .3f} {code} за 1 USD")
             else:
                 mb.showerror("Ошибка", f"Валюта {code} не найдена")
         except Exception as e:
