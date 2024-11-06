d59c8f754686b5506aa17a4e24d526718248da1c
Stas <yatouag@gmail.com>
2024-11-06 12:17:15 +0300

    Созданна функция exchage для получения из интернета курса валют.

--- Obmen.py
+++ Obmen.py
@@ -4,7 +4,23 @@ from tkinter import *
 from tkinter import messagebox as mb
 
 
-def
+def exchange():
+    code = entry.get()
+
+    if code:
+        try:
+            response = requests.get("https://open.er-api.com/v6/latest/USD")
+            response.raise_for_status()
+            data = response.json()
+            if code in data["rates"]:
+                exchange_rate = data["rates"][code]
+                mb.showinfo("Курс обмена", f"Курс: {exchange_rate} {code} за 1 USD")
+            else:
+                mb.showerror("Ошибка", f"Валюта {code} не найдена")
+        except Exception as e:
+            mb.showerror("Ошибка", f"Произошла ошибка: {e}.")
+    else:
+        mb.showwarning("Внимание!", "Введите код валюты")
 
 window = Tk()
 window.title("Курс обмена валют")
